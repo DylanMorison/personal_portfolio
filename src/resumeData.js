0a1e8b3b7a967418340d54322a04e721b2e9e86f
Dylan Morison <dylan@g.morison.org>
2020-11-23 18:49:05 -0800

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,98 @@
+let resumeData = {
+	imagebaseurl: "https://rbhatia46.github.io/",
+	name: "Dylan Morison",
+	role: "Welcome to my portfolio website! My name is Dylan and I am an aspiring full stack web developer. I have experience working with MongoDB, MySQL, Express, Node Js, React, and Redux. I am fascinated by cyber security and authentication related technologies.",
+	linkedinId: "https://www.linkedin.com/in/dylan-morison-5b5b49132/",
+	skypeid: "Your skypeid",
+	roleDescription:
+		"",
+	socialLinks: [
+		{
+			name: "linkedin",
+			url: "https://www.linkedin.com/in/dylan-morison-5b5b49132/",
+			className: "fa fa-linkedin"
+		},
+		{
+			name: "github",
+			url: "https://github.com/DylanMorison",
+			className: "fa fa-github"
+		},
+
+	],
+	aboutme:
+		"I am currently graduating from Occidental College with a Math and Computer Science major. My favorite courses in college include Number Theory, Databases, and Geometrical Optimization. I want to work as a full stack developer focused on back end related technologies, such as MongoDB/MySql, Express, and authentication.  My favorite programming languages are javascript and python.",
+	address: "Los Angeles, United States",
+	website: "dylan@g.morison.org",
+	education: [
+		{
+			UniversityName: "Occidental College",
+			specialization: "Computer Science and Mathematics",
+			MonthOfPassing: "Dec",
+			YearOfPassing: "2020",
+			Achievements: ""
+		}
+	],
+	work: [
+		{
+			CompanyName: "Senior Comprehensive Project",
+			specialization: "MERN Stack website",
+			MonthOfLeaving: "Dec",
+			YearOfLeaving: "2020",
+			Achievements: "https://time-tracker-for-students.herokuapp.com/"
+		},
+
+	],
+	skillsDescription: "",
+	skills: [
+		{
+			skillname: "Redux"
+    },
+    {
+			skillname: "MongoDB"
+    },
+    {
+			skillname: "MySQL"
+		},
+		{
+			skillname: "Express"
+		},
+		{
+			skillname: "React"
+		},
+		{
+			skillname: "Node Js"
+		},
+		{
+			skillname: "Python"
+		}
+	],
+	portfolio: [
+		{
+			name: "A multi purpose time tracker for students (1)",
+			description: "webapp",
+			imgurl: "images/timetracker.png"
+		},
+		{
+			name: "A multi purpose time tracker for students (2)",
+			description: "webapp",
+			imgurl: "images/timetracker2.png"
+		},
+		{
+			name: "Chat App",
+			description: "web app",
+			imgurl: "images/chat.png"
+		},
+	],
+	testimonials: [
+		{
+			description: "Did you debug it?",
+			name: "My Dad"
+		},
+		{
+			description: "That's trivial!",
+			name: "My dad"
+		}
+	]
+};
+
+export default resumeData;
